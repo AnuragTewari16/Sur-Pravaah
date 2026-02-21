adb0a0650874077df29a6048c2a0c7579fba6e89
Anurag Tewari <tewariventures@outlook.com>
2026-02-22 00:14:15 +0100

    Update Teachers.jsx
    height

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -23,7 +23,7 @@ function TeacherCard({ teacher }) {
       <div className="absolute top-0 left-0 right-0 h-0.5 bg-gradient-to-r from-gold via-maroon to-gold" />
 
       {/* Photo */}
-      <div className="relative w-full md:w-96 flex-shrink-0 overflow-hidden" style={{ minHeight: '280px' }}>
+      <div className="relative w-full md:w-96 flex-shrink-0 overflow-hidden" style={{ minHeight: '420px' }}>
         <img
           src={teacher.image}
           alt={teacher.name}
